12ec359b19f6f0beb9e8f800c12cea118bd6f684
nmusaelian-rally <nik.musaelian@broadcom.com>
2020-05-12 08:45:16 -0600

    using ngrok for jira url

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const config = {
   consumerKey: "OauthKey",
   consumerPrivateKeyFile: "key/jira_privatekey.pem",
-  jiraUrl: "http://0.0.0.0:5472" 
+  jiraUrl: "http://fe9772d4.ngrok.io"
+  //jiraUrl: "http://0.0.0.0:5472" 
 };
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-require('dotenv').config()
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
 const OAuth = require('oauth').OAuth;
 const express = require('express');
 const session = require('express-session');
-const helmet  = require('helmet')
+const helmet = require('helmet');
 const config = require('./config');
 
-const privateKeyData = fs.readFileSync(config["consumerPrivateKeyFile"], "utf8");
-const consumerKey = config["consumerKey"]
-const signatureMethod = "RSA-SHA1"
-const oauthVersion = "1.0"
+const privateKeyData = fs.readFileSync(
+  config['consumerPrivateKeyFile'],
+  'utf8'
+);
+const consumerKey = config['consumerKey'];
+const signatureMethod = 'RSA-SHA1';
+const oauthVersion = '1.0';
 
 const oauthUrl = `${config.jiraUrl}/plugins/servlet/oauth/`;
-const protectedResource = `${config.jiraUrl}/rest/api/2/project`
-const port = process.env.PORT || 3000;
-const callbackUrl = `http://localhost:${port}/callback`
-const sessionSecret = process.env.SESSION_SECRET
-const sessionCookieExpireDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+//const protectedResource = `${config.jiraUrl}/rest/api/2/project`;
+const port = 9999;
+//const callbackUrl = `http://localhost:${port}/callback`;
+const callbackUrl = `http://tenfoot-build-01.f4tech.com:${port}/callback`;
+const sessionSecret = `secret ${Math.random()}`;
+const sessionCookieExpireDate = new Date(
+  new Date().setFullYear(new Date().getFullYear() + 1)
+);
 
-// monkey-patch OAuth.get: 
+// monkey-patch OAuth.get:
 // In oauth library code, OAuth.get calls _performSecureRequest with next-to-last argument null ,
 // which results is content-type defaulting to "application/x-www-form-urlencoded",
 // Jira requires "application/json" hence it is explicitly passed
- 
-OAuth.prototype.get = function (url, oauth_token, oauth_token_secret, callback, post_content_type) {
-  return this._performSecureRequest(oauth_token, oauth_token_secret, "GET", url, null, "", post_content_type, callback);
+
+OAuth.prototype.get = function (
+  url,
+  oauth_token,
+  oauth_token_secret,
+  callback,
+  post_content_type
+) {
+  return this._performSecureRequest(
+    oauth_token,
+    oauth_token_secret,
+    'GET',
+    url,
+    null,
+    '',
+    post_content_type,
+    callback
+  );
 };
 // end monkey-patch
 
@@ -38,19 +61,21 @@ const consumer = new OAuth(
   signatureMethod
 );
 
-
 const app = express();
-app.use(helmet())
-app.use(session({
-  secret: sessionSecret,
-  resave: false,
-  saveUninitialized: false, //don't save empty sessions
-  cookie: {
-    secure: false,         //not limited to https for now
-    httpOnly: true,        //cookie is sent only over HTTP(S), not client js, helps protect against XSS
-    expires: sessionCookieExpireDate
-  }
-}));
+app.set('port', port);
+app.use(helmet());
+app.use(
+  session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: false, //don't save empty sessions
+    cookie: {
+      secure: false, //not limited to https for now
+      httpOnly: true, //cookie is sent only over HTTP(S), not client js, helps protect against XSS
+      expires: sessionCookieExpireDate,
+    },
+  })
+);
 app.use((req, res, next) => {
   res.session = req.session;
   next();
@@ -59,16 +84,16 @@ app.use((req, res, next) => {
 app.get('/', (request, response) => response.send('ok'));
 app.get('/connect', (request, response) => {
   consumer.getOAuthRequestToken((error, oauthToken, oauthTokenSecret) => {
-      if (error) {
-        response.send(`Error getting Request token. ${error}`);
-      }
-      else {
-        request.session.oauthRequestToken = oauthToken;
-        request.session.oauthRequestTokenSecret = oauthTokenSecret;
-        response.redirect(`${oauthUrl}authorize?oauth_token=${request.session.oauthRequestToken}`);
-      }
+    if (error) {
+      response.send(`Error getting Request token. ${error}`);
+    } else {
+      request.session.oauthRequestToken = oauthToken;
+      request.session.oauthRequestTokenSecret = oauthTokenSecret;
+      response.redirect(
+        `${oauthUrl}authorize?oauth_token=${request.session.oauthRequestToken}`
+      );
     }
-  );
+  });
 });
 
 app.get('/callback', (request, response) => {
@@ -76,34 +101,14 @@ app.get('/callback', (request, response) => {
     request.session.oauthRequestToken,
     request.session.oauthRequestTokenSecret,
     request.query.oauth_verifier,
-    (error, oauthAccessToken, oauthAccessTokenSecret) => {
+    (error, oauthAccessToken) => {
       if (error) {
-        response.send("error getting Access token");
-      }
-      else {
-        console.log('CONSUMER KEY: ', consumerKey)
-        console.log('TOKEN:', oauthAccessToken)
-        console.log('SIGNATURE METHOD: ', signatureMethod, ', OAUTH VERSION: ', oauthVersion)
-        console.log('PRIVATE KEY:\n', privateKeyData)
-
-        request.session.oauthAccessToken = oauthAccessToken;
-        request.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-        consumer.get(protectedResource,
-        request.session.oauthAccessToken,
-          request.session.oauthAccessTokenSecret,
-          function (error, data) {
-            if (error) throw error;
-            data = JSON.parse(data);
-            console.log(`${protectedResource} returned ${data.length} items.`)
-            response.send(`Projects: ${data.map(proj => 
-              '<li>key: ' + proj['key'] + ', name:' + proj['name'] + ', id:' + proj['id'] + '<\li>').join('')}`);
-          },
-          "application/json"
-        );
+        response.send(`Error getting Access token. ${error}`);
+      } else {
+        response.send(`OAUTH TOKEN: ${oauthAccessToken}`);
       }
     }
   );
 });
-
 
 app.listen(port);
